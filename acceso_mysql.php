96c0bcad45594a366b6cca282368ebfbb70acaeb
ProfeFernando <39655259+ProfeFernando@users.noreply.github.com>
2024-01-10 13:50:16 +0100

    Update acceso_mysql.php

--- acceso_mysql.php
+++ acceso_mysql.php
@@ -3,7 +3,7 @@
 $bd_usuario="user"; 
 $bd_password="password"; 
 try{ 
-   $dsn="mysql:host=192.168.138.250;port=33060;dbname=2daw"; 
+   $dsn="mysql:host=192.168.138.250;port=33060;dbname=kartBBDD"; 
    $dbh=new PDO($dsn,$bd_usuario,$bd_password); 
    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 } catch(PDOException $e){ 
