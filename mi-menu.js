bcc45f82cf553806393eff32e121b2c62119c02d
ProfeFernando <39655259+ProfeFernando@users.noreply.github.com>
2024-01-10 14:02:25 +0100

    Add files via upload

--- mi-menu.js
+++ mi-menu.js
@@ -76,44 +76,44 @@ class MiMenu extends HTMLElement {
             <li><a href="#">Coches <i class="down"></i></a>
                 <!-- Primer Menu Desplegable -->
                 <ul>
-                    <li><a href="coches.html">Alta</a></li>
-                    <li><a href="coches.html">Baja</a></li>
-                    <li><a href="coches.html">Consulta</a></li>
-                    <li><a href="coches.html">Modificación</a></li>
+                    <li><a href="coches.html?alta">Alta</a></li>
+                    <li><a href="coches.html?baja">Baja</a></li>
+                    <li><a href="coches.html?consulta">Consulta</a></li>
+                    <li><a href="coches.html?modificacion">Modificación</a></li>
                 </ul>
             </li>
             <li><a href="#">Pilotos <i class="down"></i></a>
                 <ul>
-                    <li><a href="pilotos.html">Alta</a></li>
-                    <li><a href="pilotos.html">Baja</a></li>
-                    <li><a href="pilotos.html">Consulta</a></li>
-                    <li><a href="pilotos.html">Modificación</a></li>
-                    <li><a href="pilotos.html">Facturación</a></li>
+                    <li><a href="pilotos.html?alta">Alta</a></li>
+                    <li><a href="pilotos.html?baja">Baja</a></li>
+                    <li><a href="pilotos.html?consulta">Consulta</a></li>
+                    <li><a href="pilotos.html?modifica">Modificación</a></li>
+                    <li><a href="pilotos.html?factura">Facturación</a></li>
                 </ul>
             </li>
             <li><a href="#">Empleados <i class="down"></i></a>
                 <ul>
-                    <li><a href="empleados.html">Alta</a></li>
-                    <li><a href="empleados.html">Baja</a></li>
-                    <li><a href="empleados.html">Consulta</a></li>
-                    <li><a href="empleados.html">Modificación</a></li>
+                    <li><a href="empleados.html?alta">Alta</a></li>
+                    <li><a href="empleados.html?baja">Baja</a></li>
+                    <li><a href="empleados.html?consulta">Consulta</a></li>
+                    <li><a href="empleados.html?modifica">Modificación</a></li>
                 </ul>
             </li>
             <li><a href="#">Carreras <i class="down"></i></a>
                 <ul>
-                    <li><a href="carreras.html">Alta</a></li>
-                    <li><a href="carreras.html">Baja</a></li>
-                    <li><a href="carreras.html">Consulta</a></li>
-                    <li><a href="carreras.html">Modificación</a></li>
-                    <li><a href="carreras.html">Carrera Actual</a></li>
+                    <li><a href="carreras.html?alta">Alta</a></li>
+                    <li><a href="carreras.html?baja">Baja</a></li>
+                    <li><a href="carreras.html?consulta">Consulta</a></li>
+                    <li><a href="carreras.html?modifica">Modificación</a></li>
+                    <li><a href="carreras.html?actual">Carrera Actual</a></li>
                 </ul>
             </li>
             <li><a href="#">Pilotos Carreras <i class="down"></i></a>
                 <ul>
-                    <li><a href="pilotosCarreras.html">Alta</a></li>
-                    <li><a href="pilotosCarreras.html">Baja</a></li>
-                    <li><a href="pilotosCarreras.html">Consulta</a></li>
-                    <li><a href="pilotosCarreras.html">Modificación</a></li>
+                    <li><a href="pilotosCarreras.html?alta">Alta</a></li>
+                    <li><a href="pilotosCarreras.html?baja">Baja</a></li>
+                    <li><a href="pilotosCarreras.html?consulta">Consulta</a></li>
+                    <li><a href="pilotosCarreras.html?modifica">Modificación</a></li>
                 </ul>
             </li>
         </ul>
